Fix query weight calculation

ir_model.py
@@ -159,6 +159,9 @@ class IRModel:
         typer.echo("Building inverse document frequency array...")
         self.idf = np.log(len(self.docs) / (freq > 0).sum(axis=0))
 
+        smooth_a = self.config["query_alpha_smoothing"]
+        self.idf = (1 - smooth_a) * self.idf + smooth_a
+
         # Build TF-IDF matrix
         typer.echo("Building TF-IDF matrix...")
         tf_idf = norm_freq * self.idf
@@ -218,7 +221,7 @@ class IRModel:
                 to_remove += string.punctuation
             tokens = [token for token in tokens if token not in to_remove]
             return tokens
-        
+
         return tokenization_func
 
     def _get_model_file(self, file_name: str, ext: str = "npy") -> np.ndarray:
@@ -342,7 +345,13 @@ class IRModel:
         q_words_counter = Counter(q_words)
         for word in q_words:
             q_vector[self.words_idx[word]] = q_words_counter[word]
+
+        matches = (self.freq > 0) * (q_vector > 0)
+        if np.max(q_vector) == 0:
+            return QueryResult(query, [])
+        l_term = np.log(len(self.metadata) / (np.sum(matches, axis=0) + 1))
         q_vector = smooth_a + (1 - smooth_a) * (q_vector / np.max(q_vector))
+        q_vector *= l_term
 
         # Calculate TF-IDF scores for each document
         similarty = np.array(